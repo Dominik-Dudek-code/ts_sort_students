415173bb4bc42181dd032f95cceaa074d0a182fb
Dominik Dudek <dom-dud774@o2.pl>
2024-11-09 00:27:01 +0100

    Task solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,57 @@
-
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname: string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade'
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[], sortBy: SortType, order: SortOrder,
+): Student[] {
+  function calculateAverage(grade: number[]): number {
+    return grade.reduce((acc, note) => acc + note, 0) / grade.length;
+  }
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  const studentsCopy: Student[] = [...students]
+    .sort((a: Student, b: Student): number => {
+      const averageA = calculateAverage(a.grades);
+      const averageB = calculateAverage(b.grades);
+
+      let comparison = 0;
+
+      switch (sortBy) {
+        case SortType.Name:
+          comparison = a.name.localeCompare(b.name);
+          break;
+        case SortType.Surname:
+          comparison = a.surname.localeCompare(b.surname);
+          break;
+        case SortType.Age:
+          comparison = a.age - b.age;
+          break;
+        case SortType.Married:
+          comparison = Number(a.married) - Number(b.married);
+          break;
+        case SortType.AverageGrade:
+          comparison = averageA - averageB;
+          break;
+        default:
+          throw new Error('Invalid sort type');
+      }
+
+      return order === 'asc' ? comparison : -comparison;
+    });
+
+  return studentsCopy;
 }
